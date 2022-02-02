2a28e37f8f829743862fc00783efd022c4d3b726
pallomagualter <pallomagualter@gmail.com>
2022-02-02 17:00:44 -0300

    correção chamada theme e SignIn

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,6 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin',
       [
         'module-resolver',
         {
@@ -18,9 +17,12 @@ module.exports = function(api) {
             '@components' : './src/components',
             '@screens' : './src/screens',
             '@assets' : './src/assets',
+            '@hooks' : './src/hooks',
+            '@utils' : './src/utils',
           }
         }
       ]
+    
     ]
   };
 };
