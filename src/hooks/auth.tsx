fdd1f4fd1ea193314025a303e9ea75a0f8dc8233
pallomagualter <pallomagualter@gmail.com>
2022-02-15 09:28:42 -0300

    auth

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,30 @@
+import React, {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+} from 'react';
+
+type AuthContextData = {
+
+}
+
+type AuthProviderProps = {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+function AuthProvider({ children }: AuthProviderProps) {
+  <AuthContext.Provider value={{}}>
+    {children}
+  </AuthContext.Provider>
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
